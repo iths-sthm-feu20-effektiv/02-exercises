bb1ad9b5d6ddfe5ae3ab4bb9c6a14c88a28c6099
David Andersson <lejonmanen@gmail.com>
2021-04-01 11:53:58 +0200

    Added comments

--- sanitize.js
+++ sanitize.js
@@ -1,10 +1,14 @@
+// Importera npm-paketet vi ska använda
 const sanitizeHtml = require("sanitize-html")
 
 console.log('Sanitize some dirty HTML');
 
+// Text som innehåller HTML
 const dirty = '<h1 class="top"> Heading </h1> Today is a <em id="sun">sunny</em> day.';
 
+// Använd npm-paketet för att rensa bort HTML från strängen
 const clean = sanitizeHtml(dirty, { allowedTags: [], allowedAttributes: {} })
 
+// Skriv ut resultatet: före och efter
 console.log(dirty);
 console.log(clean);
